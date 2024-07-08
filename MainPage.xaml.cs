f17e52d94addc1f92fd3627923a405d63621367a
okafors <102932325+okafors@users.noreply.github.com>
2024-07-08 13:15:01 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
+//added new features
+
+
+
+//created the screen and design
